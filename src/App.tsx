b9f0add4f8585185fb838badf2acb153e283a8bc
Can Usluel <41291070+canusluel@users.noreply.github.com>
2022-08-12 14:15:22 +0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react';
+import LoginScreen from './components/LoginScreen';
+import 'antd/dist/antd.min.css';
+
+
+const App: FC = () => {
+  return (
+    <div className="App">
+     <LoginScreen />
+    </div>
+  );
+}
+
+export default App;
