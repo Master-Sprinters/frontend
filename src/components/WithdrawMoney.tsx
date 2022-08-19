692a6b5d1cf982f034b00fb3f0a851c11930c41d
kaanbozdogan <32725324+kaanbozdogan@users.noreply.github.com>
2022-08-19 13:05:37 +0300

    WithdrawMoney ve ChildScreen eklendi.

---
+++ src/components/WithdrawMoney.tsx
@@ -0,0 +1,183 @@
+import React, {useState} from "react";
+import {FunctionComponent} from "react";
+import {Button, Form, InputNumber, Space, DatePicker} from "antd";
+import {Row, Col, Popconfirm, message, Input, Select} from "antd";
+import moment from "moment";
+import "../styles.css"
+
+
+type Params = {
+    _name: string;
+    _surname: string;
+    _accId: string;
+    _transferDate: string;
+    _budget: string;
+    _isParentAcc: boolean;
+}
+
+const WithdrawMoney: FunctionComponent<Params> = ({_name, _surname, _accId, _transferDate, _budget, _isParentAcc}: Params) => {
+
+    const [name, setName] = useState<string>(_name)
+    const [surname, setSurname] = useState<string>(_surname)
+    const [accId, setAccId] = useState<string>(_accId)
+    const [transferDate, setTransferDate] = useState<string>(_transferDate)
+    const [budget, setBudget] = useState<string>(_budget)
+    
+    const [newBudget, setNewBudget] = useState<string>("")
+    const [newDate, setNewDate] = useState<string>("")
+
+    const dateFormatList = ['DD/MM/YYYY'];
+
+
+    const returnRow = (leftStr: string, rightStr: string) => {
+        return (
+            <Form.Item
+                label={leftStr}
+                name="amount"
+                labelAlign="right"
+                className="child-text"
+            >
+                {rightStr}
+            </Form.Item>
+        );
+    }
+
+    const withdrawConfirm = () => {
+        setBudget("0 ETH");
+        message.info('Para hesabınıza çekildi.');
+    }
+
+    const saveConfirm = () => {
+        message.info('Yeni bilgiler kaydedildi.');
+    }
+
+    const onClickSave = () => {
+        console.log("save button clicked")
+
+        if (window.confirm('Yeni bilgileri kaydetmek istediğinize emin misiniz?')) {
+            console.log("save button: accepted")
+            setBudget("100 ETH")
+        }
+        else {
+            console.log("save button: rejected")
+        }
+    }
+
+    const onClickWithdraw = () => {
+        console.log("save button clicked")
+
+        if (window.confirm('Varlığı hesabınıza çekmek istediğinize emin misiniz?')) {
+            console.log("save button: accepted")
+            setBudget("0 ETH")
+        }
+        else {
+            console.log("save button: rejected")
+        }
+    }
+
+    const fromLayout = {
+        labelCol: { span: 10 },
+        wrapperCol: { span: 13, offset: 1 },
+      };
+
+    const onBudgetChange = (e: number) => {
+        console.log(e)
+    }
+
+
+    return (
+        <div>
+            {_isParentAcc && 
+                <div>
+                    <Row justify="center" className="child-header" style={{paddingTop: "75px", paddingBottom: "50px"}}>
+                        Varlık ve Devir Tarihini Düzenle
+                    </Row>
+
+                    <Form {...fromLayout}
+                        name="basic"
+                        initialValues={{ remember: true }}
+                        autoComplete="off"
+                        colon={false}
+                    >
+                        {returnRow("İsim Soyisim :", name+" "+surname)}
+                        {returnRow("Hesap ID :", accId)}
+                        {returnRow("Devir Tarihi :", transferDate)}
+                        {returnRow("Varlık Miktarı :", budget)}
+
+                        <Form.Item
+                            label="Yeni Miktar :"
+                            name="amount"
+                            rules={[{ required: true, message: 'Lütfen miktarı giriniz.' }]}
+                            labelAlign="right"
+                        >
+                            <InputNumber
+                                id="transfer-amount" min={0} required={true}
+                                style={{width: "150px"}}
+                                onChange={(e) => onBudgetChange(+e.valueOf())}
+                            />
+                        </Form.Item>
+
+                        <Form.Item
+                            label="Yeni Devir Tarihi :"
+                            name="date"
+                            labelAlign="right"
+                        >
+                            <DatePicker format={dateFormatList} style={{width: "150px"}}
+                                    defaultValue={moment(transferDate, dateFormatList)}
+                                    allowClear={false}
+                                />
+                        </Form.Item>
+
+                        <Form.Item
+                            wrapperCol={{ ...fromLayout.wrapperCol, offset: 11 }}
+                            style={{paddingTop: "20px"}}
+                        >
+                            <Button id="child-withdraw-btn" type="primary" htmlType="submit"
+                                className="std-button" style={{width: "120px", height: "60px"}}
+                                onClick={onClickSave}
+                            >
+                                Kaydet
+                            </Button>
+                        </Form.Item>
+
+                    </Form>
+                </div>
+            }
+            
+            {!_isParentAcc &&
+                <div>
+                    <Row justify="center" className="child-header" style={{paddingTop: "75px", paddingBottom: "50px"}}>
+                        Para Çek
+                    </Row>
+                    <Form {...fromLayout}
+                        name="basic"
+                        initialValues={{ remember: true }}
+                        autoComplete="off"
+                        colon={false}
+                    >
+                        {returnRow("İsim Soyisim :", name+" "+surname)}
+                        {returnRow("Hesap ID :", accId)}
+                        {returnRow("Devir Tarihi :", transferDate)}
+                        {returnRow("Varlık Miktarı :", budget)}
+
+                        <Form.Item
+                            wrapperCol={{ ...fromLayout.wrapperCol, offset: 11 }}
+                            style={{paddingTop: "20px"}}
+                        >
+                            <Button id="child-withdraw-btn" type="primary"
+                                className="std-button" style={{width: "140px", height: "70px"}}
+                                onClick={onClickWithdraw}
+                            >
+                                Parayı Çek
+                            </Button>
+                        </Form.Item>
+
+                    </Form>
+                </div>
+            }
+            
+        </div>
+    );
+}
+
+export default WithdrawMoney;
--- src/pages/ChildScreen.tsx
+++ src/pages/ChildScreen.tsx
@@ -1,9 +1,11 @@
 import React, {useState} from "react";
 import {FC} from "react";
 import {Button} from "antd";
-import {Row, Col, Space} from "antd";
+import {Row, Col, Popconfirm, message} from "antd";
+import WithdrawMoney from '../components/WithdrawMoney'
 
-const ChildScreen: FC = () => {
+
+const ChildScreen = () => {
 
     const [name, setName] = useState<string>("Kaan Can")
     const [surname, setSurname] = useState<string>("Bozdoğan")
@@ -12,40 +14,9 @@ const ChildScreen: FC = () => {
     const [budget, setBudget] = useState<string>("123 ETH")
 
 
-    const returnRow = (leftStr: string, rightStr: string) => {
-        return (
-            <Row justify="center" className="child-text" style={{paddingBottom: "15px"}}>
-                <Col span={10} style={{textAlign: "right"}} >
-                    {leftStr}
-                </Col>
-                <Col span={13} offset={1} style={{textAlign: "left"}}>
-                    {rightStr}
-                </Col>
-            </Row>
-        );
-    }
-
     return (
-        <div>
-            <Row
-                justify="center" className="child-header"
-                style={{paddingTop: "90px", paddingBottom: "60px"}}
-            >
-                Para Çek
-            </Row>   
-            {returnRow("İsim Soyisim :", name+" "+surname)}
-            {returnRow("Hesap ID :", accId)}
-            {returnRow("Devir Tarihi :", transferDate)}
-            {returnRow("Varlık Miktarı :", budget)}
-            <Row justify="center" style={{paddingTop: "60px"}}>
-                <Button id="child-withdraw-btn" type="primary" className="std-button">
-                    Çek
-                </Button>
-            </Row> 
-            
-        </div>
-    );
-
+        <WithdrawMoney _isParentAcc={false} _name={name} _surname={surname} _accId={accId} _transferDate={transferDate} _budget={budget}/>
+    )
 }
 
 export default ChildScreen;
