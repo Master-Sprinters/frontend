1e11f7b70c4b732d072593a8b99477c0a6d251ff
Can Usluel <canusluel@gmail.com>
2022-09-02 10:04:31 +0300

    added logo for browser

--- src/pages/ParentScreen.tsx
+++ src/pages/ParentScreen.tsx
@@ -190,7 +190,7 @@ import { ethers } from 'ethers';
         contract.getParent()
           .then(async (res: any) => {
             const setMainMenu = [
-              <MainMenu clientName={res.name} clientAddress={res._address} connectProvider={connectProvider}/>
+              <MainMenu key={15} clientName={res.name} clientAddress={res._address} connectProvider={connectProvider}/>
             ]
             
             setCurrentScreen(setMainMenu)
